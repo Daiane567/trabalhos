e157b27a5665a8fd71600ee0cf7d8664602d0808
Daiane567 <dr465796@gmail.com>
2021-02-18 10:46:23 -0300

    exercicios

---
+++ EstruturaDeRepeticao/exercicio12/script.js
@@ -0,0 +1,14 @@
+
+ 
+ 
+     var A = 1;
+ A+=5;
+ console.log(A);  
+ 
+ var A =   A+=5;
+// Faça um programa que leia 5 números e informe a soma e a média dos números.
+   for( var i=0;i<5; i+=5){
+    prompt("Digite o numero:");
+    console.log(i)
+        }
+   
