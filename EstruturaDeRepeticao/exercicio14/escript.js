a725caa0a1801d2f3941677490c54f53118767bf
Silvio Domingos <silvio.d.cardoso@gmail.com>
2021-02-20 20:44:52 +0100

    correcao + arrays

--- EstruturaDeRepeticao/exercicio14/escript.js
+++ EstruturaDeRepeticao/exercicio14/escript.js
@@ -1,9 +1,9 @@
 //Faça um programa que 
 //calcule o mostre a média aritmética de N notas. 
-notas1 = parseFloat(prompt("digite o número:"));
-notas2 = parseFloat(prompt("digite o número:"));
-var n = 0
-for (var i=0; i<10; i/n){
-   n= notas1+=notas2/n
-    
-}console.log(i)
+var n = parseFloat(prompt("Digite a quantidade de notas"));
+var soma = 0;
+for(var i=0; i<n ; i++){
+  var notas = parseFloat(prompt("Digite a nota"));
+  soma += notas;
+}
+console.log(soma/n)
--- EstruturaDeRepeticao/exercicio15/script.js
+++ EstruturaDeRepeticao/exercicio15/script.js
@@ -1,12 +1,8 @@
 //Faça um programa que calcule o mostre a média aritmética de N notas. 
-var N = parseFloat(prompt("Digite sua nota:"));
-var N =   N+=N;
-
-
-for(var i=0; i>3; i++){
-   var N = parseFloat(prompt("Digite sua nota:"));
-   
-
-    N+=N(N/2)
-    console.log(i);
+var quantidade = parseFloat(prompt("Digite a quantidade de Notas"));
+var soma = 0;
+for(var i=0; i<quantidade; i++){
+   var notas = parseFloat(prompt("Digite sua nota:"));
+   soma += notas
 }
+console.log(soma/quantidade)
