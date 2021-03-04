6de48e6b6e738c9a79b06311fda75398d0b835bd
Daiane567 <dr465796@gmail.com>
2021-03-04 17:25:19 -0300

    array

---
+++ Array/exercicio2/script.js
@@ -0,0 +1,24 @@
+//Crie um programa que dado o array:
+//[32, 50, 61, 86, -45, 96, -30, 4, 76, 36, 38, 18, 77, 35, 22, 1, -85, 28, 10, 45]
+//Mostre no final a quantidade de elementos do array que são números negativos
+//Mostre no final a quantidade de elementos do array que são 
+//divisíveis por 2
+//Mostre no final a quantidade de elementos do array que são divisíveis por 3
+var numeros = [32, 50, 61, 86, -45, 96, -30, 4, 76, 36, 38, 18, 77, 35, 22, 1, -85, 28, 10, 45];
+total = 0;
+negativo = 0;
+soma1 = 0;
+for(var i = 0; i<numeros.length; i++){
+    negativo = numeros[i];
+    soma1+=i;
+    if(negativo<0){
+        total = negativo
+        console.log(numeros[i]);
+    }
+    if(numeros>0){
+        numeros/2
+        console.log(numeros[i]);
+    }
+
+
+}
